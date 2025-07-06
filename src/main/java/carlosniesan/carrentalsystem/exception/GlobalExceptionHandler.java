35bf1da261047bff812ffbae1e031862557e8a85
Carlos <carlosniesan@outlook.com>
2025-07-07 00:27:37 +0200

    Mejoras

--- src/main/java/carlosniesan/carrentalsystem/exception/GlobalExceptionHandler.java
+++ src/main/java/carlosniesan/carrentalsystem/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
+@ControllerAdvice
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     
